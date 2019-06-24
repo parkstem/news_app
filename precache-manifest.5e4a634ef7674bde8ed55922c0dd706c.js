2f35c7716a3ce7388bdf57c05704dd2ac3daca66
parkstem <qkrwnfrl@naver.com>
2019-06-24 20:51:06 +0900

    Updates

---
+++ precache-manifest.5e4a634ef7674bde8ed55922c0dd706c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46c5c89555d7c9e88d045bcee4c7835",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "098170e6a4ec14e3b9ec",
+    "url": "/news_app/static/css/main.d48bdf95.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/news_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "098170e6a4ec14e3b9ec",
+    "url": "/news_app/static/js/main.14fa2f51.chunk.js"
+  },
+  {
+    "revision": "5406e2c00fc9f3d44090",
+    "url": "/news_app/static/js/runtime~main.a6e64a1f.js"
+  }
+]);
